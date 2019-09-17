155958253f9a93ced9d579b282615340b3c06ee3
Abdullah Al Raqibul Islam <iraqibul.csedu@gmail.com>
2019-09-16 21:01:37 -0400

    Add runtime measuring block in the template code

--- project_1/template.cpp
+++ project_1/template.cpp
@@ -18,11 +18,12 @@
 #include <deque>
 #include <queue>
 #include <stack>
+
 using namespace std;
 
 //#define _rep( i, a, b, x ) for( __typeof(b) i = ( a ); i <= ( b ); i += x )
-#define _rep( i, a, b, x ) for( i = ( a ); i <= ( b ); i += x )
-#define rep( i, n ) _rep( i, 0, n - 1, 1 )
+#define _rep(i, a, b, x) for( i = ( a ); i <= ( b ); i += x )
+#define rep(i, n) _rep( i, 0, n - 1, 1 )
 
 #define ff first
 #define ss second
@@ -43,15 +44,23 @@ typedef long double ld;
 //const double pi = (2.0*acos(0.0));
 const double pi = acos(-1.0);
 const double eps = 1e-9;
-const int inf = (1<<28);
+const int inf = (1 << 28);
 const int MAX = 1005;
 
 int main() {
-	//freopen("in.txt", "r", stdin);
-	//freopen("out.txt", "w", stdout);
+    //freopen("in.txt", "r", stdin);
+    //freopen("out.txt", "w", stdout);
 
-	int i, j, k;
-	int test, t = 0, kase=0;
+    int i, j, k;
+    int test, t = 0, kase = 0;
 
-	return 0;
+    //load input after this line
+
+    double st = clock();
+
+    //note: implement your algorithm here ... execution time will be printed automatically!
+
+    cerr << (clock() - st) / CLOCKS_PER_SEC << endl;
+
+    return 0;
 }
