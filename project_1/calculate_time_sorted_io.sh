cdf0415b75344abdfeab447e862337aca1d46269
Abdullah Al Raqibul Islam <iraqibul.csedu@gmail.com>
2019-09-22 19:44:07 -0400

    Add execution time calculating script for different dataset

---
+++ project_1/calculate_time_sorted_io.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+#echo "Algorithm: Insertion Sort"
+#echo "Input-set: sorted"
+input_path="input/sorted/"
+g++ -o insertion_sort insertion_sort.cpp
+for file in input/rev_sorted/*; do
+  #echo "Input File: ${file##*/}"
+  counter=1
+  execution_time=0.0
+  while [ $counter -le 3 ]
+  do
+    ./insertion_sort < $input_path${file##*/} > output.txt
+    ((counter++))
+  done
+done
+
+#echo "*****************<>*****************"
+
+#echo "Algorithm: Merge Sort"
+#echo "Input-set: sorted"
+g++ -o merge_sort merge_sort.cpp
+for file in input/rev_sorted/*; do
+  #echo "Input File: ${file##*/}"
+  counter=1
+  execution_time=0.0
+  while [ $counter -le 3 ]
+  do
+    ./merge_sort < $input_path${file##*/} > output.txt
+    ((counter++))
+  done
+done
+
+#echo "*****************<>*****************"
+
+#echo "Algorithm: Heap Sort"
+#echo "Input-set: sorted"
+g++ -o heapsort heapsort.cpp
+for file in input/rev_sorted/*; do
+  #echo "Input File: ${file##*/}"
+  counter=1
+  execution_time=0.0
+  while [ $counter -le 3 ]
+  do
+    ./heapsort < $input_path${file##*/} > output.txt
+    ((counter++))
+  done
+done
+
+#echo "*****************<>*****************"
+
+#echo "Algorithm: In-place Quick Sort"
+#echo "Input-set: sorted"
+g++ -o inplace_quicksort inplace_quicksort.cpp
+for file in input/rev_sorted/*; do
+  #echo "Input File: ${file##*/}"
+  counter=1
+  execution_time=0.0
+  while [ $counter -le 3 ]
+  do
+    ./inplace_quicksort < $input_path${file##*/} > output.txt
+    ((counter++))
+  done
+done
+
+#echo "*****************<>*****************"
+
+#echo "Algorithm: Modified Quick Sort"
+#echo "Input-set: sorted"
+g++ -o modified_quicksort modified_quicksort.cpp
+for file in input/rev_sorted/*; do
+  #echo "Input File: ${file##*/}"
+  counter=1
+  execution_time=0.0
+  while [ $counter -le 3 ]
+  do
+    ./modified_quicksort < $input_path${file##*/} > output.txt
+    ((counter++))
+  done
+done
