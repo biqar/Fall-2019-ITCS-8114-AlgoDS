8f7b69f30b50bf22eaa3bfe3ca9dd97e9c56ccd4
Abdullah Al Raqibul Islam <iraqibul.csedu@gmail.com>
2019-09-21 21:42:03 -0400

    Update modified quicksort
    Added call to insertion_sort for small sub-problem of size less than or equal to 10.

--- project_1/modified_quicksort.cpp
+++ project_1/modified_quicksort.cpp
@@ -18,11 +18,12 @@
 #include <deque>
 #include <queue>
 #include <stack>
+
 using namespace std;
 
 //#define _rep( i, a, b, x ) for( __typeof(b) i = ( a ); i <= ( b ); i += x )
-#define _rep( i, a, b, x ) for( i = ( a ); i <= ( b ); i += x )
-#define rep( i, n ) _rep( i, 0, n - 1, 1 )
+#define _rep(i, a, b, x) for( i = ( a ); i <= ( b ); i += x )
+#define rep(i, n) _rep( i, 0, n - 1, 1 )
 
 #define ff first
 #define ss second
@@ -43,15 +44,15 @@ typedef long double ld;
 //const double pi = (2.0*acos(0.0));
 const double pi = acos(-1.0);
 const double eps = 1e-9;
-const int inf = (1<<28);
+const int inf = (1 << 28);
 const int MAX = 50005;
 
 int n;
 int input_arr[MAX];
 
 int median_of_three(int a, int b, int c) {
-    if((b <= a && a <= c) || (c <= a && a <= b)) return a;
-    if((a <= b && b <= c) || (c <= b && b <= a)) return b;
+    if ((b <= a && a <= c) || (c <= a && a <= b)) return a;
+    if ((a <= b && b <= c) || (c <= b && b <= a)) return b;
     return c;
 }
 
@@ -60,52 +61,63 @@ int get_pivot(int left, int right) {
     return median_of_three(input_arr[left], input_arr[mid], input_arr[right]);
 }
 
+int insertion_sort(int start, int end) {
+    int i, j, k;
+    for (j = start + 1; j <= end; j++) {
+        k = input_arr[j];
+        i = j - 1;
+        while (i >= 0 && input_arr[i] > k) {
+            input_arr[i + 1] = input_arr[i];
+            i--;
+        }
+        input_arr[i + 1] = k;
+    }
+    return 0;
+}
+
 void inplace_quick_sort(int left, int right) {
     //base case
-    if(left+10 <= right) {
+    if (left + 10 <= right) {
         int pivot = get_pivot(left, right);
         int i = left, j = right;
 
-        while(true) {
-            while(input_arr[i] < pivot) i+=1;
-            while(pivot < input_arr[j]) j -= 1;
-            if(i < j) {
+        while (true) {
+            while (input_arr[i] < pivot) i += 1;
+            while (pivot < input_arr[j]) j -= 1;
+            if (i < j) {
                 swap(input_arr[i], input_arr[j]);
                 i += 1;
                 j -= 1;
-            }
-            else break;
+            } else break;
         }
-        inplace_quick_sort(left, i-1);
-        inplace_quick_sort(j+1, right);
-    }
-    else {
-        //todo: @Jawad please insert the code of insertion sort
+        inplace_quick_sort(left, i - 1);
+        inplace_quick_sort(j + 1, right);
+    } else {
         insertion_sort(left, right);
     }
 }
 
 int main() {
-	//freopen("input/random/in_40k.txt", "r", stdin);
-	//freopen("out.txt", "w", stdout);
+    //freopen("input/random/in_40k.txt", "r", stdin);
+    //freopen("out.txt", "w", stdout);
 
-	int i, j, k;
-	int test, t = 0, kase=0;
+    int i, j, k;
+    int test, t = 0, kase = 0;
 
-	scanf("%d", &n);
-    for(i=0; i<n; i+=1) {
+    scanf("%d", &n);
+    for (i = 0; i < n; i += 1) {
         scanf("%d", &input_arr[i]);
     }
 
-    double st=clock();
+    double st = clock();
 
-    inplace_quick_sort(0, n-1);
+    inplace_quick_sort(0, n - 1);
 
-    for(i=0; i<n; i+=1) {
+    for (i = 0; i < n; i += 1) {
         printf("%d\n", input_arr[i]);
     }
 
-    cerr << (clock()-st)/CLOCKS_PER_SEC << endl;
+    cerr << (clock() - st) / CLOCKS_PER_SEC << endl;
 
-	return 0;
+    return 0;
 }
