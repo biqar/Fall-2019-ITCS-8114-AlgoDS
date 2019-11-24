923a717398aa6ec6ece61db154e56df564b0f12d
Abdullah Al Raqibul Islam <iraqibul.csedu@gmail.com>
2019-11-24 14:39:35 -0500

    Add brute force pattern matching in project_3
    Also updated the sample input.

--- project_3/src/1_brute_force.cpp
+++ project_3/src/1_brute_force.cpp
@@ -6,46 +6,26 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cmath>
-#include <cctype>
 #include <cstring>
-
 #include <string>
 #include <algorithm>
-#include <vector>
-#include <list>
-#include <map>
-#include <set>
-#include <deque>
-#include <queue>
-#include <stack>
-
 using namespace std;
 
-//#define _rep( i, a, b, x ) for( __typeof(b) i = ( a ); i <= ( b ); i += x )
-#define _rep(i, a, b, x) for( i = ( a ); i <= ( b ); i += x )
-#define rep(i, n) _rep( i, 0, n - 1, 1 )
-
-#define ff first
-#define ss second
-
-#define pii pair< int, int >
-#define psi pair< string, int >
-
-#define pb(x) push_back(x)
-#define mp(x, y) make_pair(x, y)
-
-#define set(p) memset(p, -1, sizeof(p))
-#define clr(p) memset(p, 0, sizeof(p))
-
-//typedef long long i64;
-//typedef __int64 i64;
 typedef long double ld;
 
-//const double pi = (2.0*acos(0.0));
-const double pi = acos(-1.0);
-const double eps = 1e-9;
 const int inf = (1 << 28);
-const int MAX = 1005;
+
+string text, pattern;
+int text_len, pattern_len;
+
+int brute_force_matching() {
+    for(int i=0; i<=(text_len-pattern_len); i+=1) {
+        int j = 0;
+        while(j<pattern_len && text[i+j] == pattern[j]) j+=1;
+        if(j == pattern_len) return i;
+    }
+    return -1;
+}
 
 int main() {
     //freopen("in.txt", "r", stdin);
@@ -55,12 +35,17 @@ int main() {
     int test, t = 0, kase = 0;
 
     //load input after this line
+    getline(cin, text);
+    getline(cin, pattern);
+    text_len = text.length();
+    pattern_len = pattern.length();
 
     double st = clock();
-
-    //note: implement your algorithm here ... execution time will be printed automatically!
-
+    int match_found = brute_force_matching();
     cerr << (clock() - st) / CLOCKS_PER_SEC << endl;
+
+    if(match_found == -1) printf("pattern not matched in the text\n");
+    else printf("pattern matched in the text at text position: %d\n", match_found);
 
     return 0;
 }
