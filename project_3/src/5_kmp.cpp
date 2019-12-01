c5d25dce0251e53b6be24d7ead9b3f7963f71f65
Abdullah Al Raqibul Islam <iraqibul.csedu@gmail.com>
2019-11-30 19:37:58 -0500

    Add kmp pattern matching implementation

--- project_3/src/5_kmp.cpp
+++ project_3/src/5_kmp.cpp
@@ -13,12 +13,52 @@ using namespace std;
 
 typedef long double ld;
 
+const int MAX = 1000005;
 const int inf = (1 << 28);
 
 string text, pattern;
 int text_len, pattern_len;
+int pi[MAX];
+
+void kmp_failure_function_naive() {
+    for(int i=0; i<pattern_len; i+=1) {
+        for(int j=i; j>=0; j-=1) {
+            if(pattern.substr(0, j) == pattern.substr(i-j+1, j)) {
+                pi[i] = j;
+                break;
+            }
+        }
+    }
+}
+
+void kmp_failure_function_fast() {
+    pi[0] = 0;
+    for(int i=1; i<pattern_len; i+=1) {
+        int j = pi[i-1];
+
+        while(j>0 && pattern[i] != pattern[j]) j = pi[j-1];
+        if(pattern[i] == pattern[j]) j += 1;
+        pi[i] = j;
+    }
+}
 
 int kmp_matching() {
+    kmp_failure_function_fast();
+    int i = 0, j = 0;
+
+    while(i < text_len) {
+        if(text[i] == pattern[j]) {
+            if(j == pattern_len-1) return i - j;
+            else {
+                i += 1;
+                j += 1;
+            }
+        }
+        else {
+            if(j > 0) j = pi[j - 1];
+            else i += 1;
+        }
+    }
     return -1;
 }
 
