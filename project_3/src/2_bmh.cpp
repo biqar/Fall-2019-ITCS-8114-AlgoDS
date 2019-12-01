09dfb291c72bde07eff49c2b4e2d6928e4d4c436
Abdullah Al Raqibul Islam <iraqibul.csedu@gmail.com>
2019-12-01 02:02:17 -0500

    Add boyer-moore-horspool pattern matching algorithm in project_3

--- project_3/src/2_bmh.cpp
+++ project_3/src/2_bmh.cpp
@@ -6,46 +6,49 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cmath>
-#include <cctype>
 #include <cstring>
-
 #include <string>
 #include <algorithm>
-#include <vector>
-#include <list>
-#include <map>
-#include <set>
-#include <deque>
-#include <queue>
-#include <stack>
-
 using namespace std;
 
-//#define _rep( i, a, b, x ) for( __typeof(b) i = ( a ); i <= ( b ); i += x )
-#define _rep(i, a, b, x) for( i = ( a ); i <= ( b ); i += x )
-#define rep(i, n) _rep( i, 0, n - 1, 1 )
+#define scale(x) (x-'A')
+#define rev_scale(x) (x+'A')
 
-#define ff first
-#define ss second
-
-#define pii pair< int, int >
-#define psi pair< string, int >
-
-#define pb(x) push_back(x)
-#define mp(x, y) make_pair(x, y)
-
-#define set(p) memset(p, -1, sizeof(p))
-#define clr(p) memset(p, 0, sizeof(p))
-
-//typedef long long i64;
-//typedef __int64 i64;
 typedef long double ld;
 
-//const double pi = (2.0*acos(0.0));
-const double pi = acos(-1.0);
-const double eps = 1e-9;
+const int MAX = 1000005;
 const int inf = (1 << 28);
-const int MAX = 1005;
+
+string text, pattern;
+int text_len, pattern_len;
+int shift_table[30];
+
+void build_shift_table() {
+    for(int i=0; i<30; i+=1) shift_table[i] = pattern_len;
+    for(int j=1,i=pattern_len-2; i>=0; j+=1,i-=1) {
+        if(shift_table[scale(pattern[i])] == pattern_len) shift_table[scale(pattern[i])] = j;
+    }
+}
+
+void print_shift_table() {
+    for(int i=0; i<26; i+=1) {
+        printf("%c:%d ", rev_scale(i), shift_table[i]);
+    }
+    printf("\n");
+}
+
+int horspool_matching() {
+    build_shift_table();
+
+    int i = pattern_len - 1;
+    while(i < text_len) {
+        int j = 0;
+        while(j<pattern_len && pattern[pattern_len-1-j] == text[i-j]) j+= 1;
+        if(j == pattern_len) return (i - pattern_len + 1);
+        i += shift_table[scale(text[i])];
+    }
+    return -1;
+}
 
 int main() {
     //freopen("in.txt", "r", stdin);
@@ -54,13 +57,17 @@ int main() {
     int i, j, k;
     int test, t = 0, kase = 0;
 
-    //load input after this line
+    getline(cin, text);
+    getline(cin, pattern);
+    text_len = text.length();
+    pattern_len = pattern.length();
 
     double st = clock();
-
-    //note: implement your algorithm here ... execution time will be printed automatically!
-
+    int match_found = horspool_matching();
     cerr << (clock() - st) / CLOCKS_PER_SEC << endl;
+
+    if(match_found == -1) printf("pattern not matched in the text\n");
+    else printf("pattern matched in the text at text position: %d\n", match_found);
 
     return 0;
 }
