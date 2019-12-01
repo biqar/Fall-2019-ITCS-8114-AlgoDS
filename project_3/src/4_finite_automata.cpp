07038564a80554fb95dd477eb6af9f79e4a830ea
Abdullah Al Raqibul Islam <iraqibul.csedu@gmail.com>
2019-12-01 01:37:27 -0500

    Add deterministic finite automation based pattern matching algorithm in project_3

--- project_3/src/4_finite_automata.cpp
+++ project_3/src/4_finite_automata.cpp
@@ -6,46 +6,64 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cmath>
-#include <cctype>
 #include <cstring>
-
 #include <string>
 #include <algorithm>
-#include <vector>
-#include <list>
-#include <map>
-#include <set>
-#include <deque>
-#include <queue>
-#include <stack>
-
 using namespace std;
 
-//#define _rep( i, a, b, x ) for( __typeof(b) i = ( a ); i <= ( b ); i += x )
-#define _rep(i, a, b, x) for( i = ( a ); i <= ( b ); i += x )
-#define rep(i, n) _rep( i, 0, n - 1, 1 )
+#define scale(x) (x-'A')
+#define rev_scale(x) (x+'A')
 
-#define ff first
-#define ss second
-
-#define pii pair< int, int >
-#define psi pair< string, int >
-
-#define pb(x) push_back(x)
-#define mp(x, y) make_pair(x, y)
-
-#define set(p) memset(p, -1, sizeof(p))
-#define clr(p) memset(p, 0, sizeof(p))
-
-//typedef long long i64;
-//typedef __int64 i64;
 typedef long double ld;
 
-//const double pi = (2.0*acos(0.0));
-const double pi = acos(-1.0);
-const double eps = 1e-9;
+const int MAX = 1000005;
 const int inf = (1 << 28);
-const int MAX = 1005;
+
+string text, pattern;
+int text_len, pattern_len;
+int dfa[30][MAX], pi[MAX];
+
+//kmp_failure_function_fast
+void prefix_function() {
+    pi[0] = 0;
+    for(int i=1; i<pattern_len; i+=1) {
+        int j = pi[i-1];
+
+        while(j>0 && pattern[i] != pattern[j]) j = pi[j-1];
+        if(pattern[i] == pattern[j]) j += 1;
+        pi[i] = j;
+    }
+}
+
+void calculate_dfa_table() {
+    prefix_function();
+    for (int i = 0; i < pattern_len; i+=1) {
+        for (int c = 0; c < 26; c+=1) {
+            if (i > 0 && rev_scale(c) != pattern[i]) dfa[c][i] = dfa[c][pi[i-1]];
+            else dfa[c][i] = i + (rev_scale(c) == pattern[i]);
+        }
+    }
+}
+
+void print_dfa_table() {
+    for(int i=0; i<26; i+=1) {
+        for(int j=0; j<pattern_len; j+=1) {
+            printf("%d ");
+        }
+        printf("\n");
+    }
+}
+
+int dfa_matching() {
+    calculate_dfa_table();
+
+    int current_state = 0;
+    for(int i=0; i<text_len; i+=1) {
+        current_state = dfa[scale(text[i])][current_state];
+        if(current_state == pattern_len) return (i - pattern_len + 1);
+    }
+    return -1;
+}
 
 int main() {
     //freopen("in.txt", "r", stdin);
@@ -54,13 +72,17 @@ int main() {
     int i, j, k;
     int test, t = 0, kase = 0;
 
-    //load input after this line
+    getline(cin, text);
+    getline(cin, pattern);
+    text_len = text.length();
+    pattern_len = pattern.length();
 
     double st = clock();
-
-    //note: implement your algorithm here ... execution time will be printed automatically!
-
+    int match_found = dfa_matching();
     cerr << (clock() - st) / CLOCKS_PER_SEC << endl;
+
+    if(match_found == -1) printf("pattern not matched in the text\n");
+    else printf("pattern matched in the text at text position: %d\n", match_found);
 
     return 0;
 }
